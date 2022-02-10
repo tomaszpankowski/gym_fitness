83954d19cb8414fda02d695eac84ce27fc123f59
tomaszpankowski <pankowski.email@gmail.com>
2022-02-10 16:11:03 +0100

    layout done

--- src/views/classes.js
+++ src/views/classes.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import ClassesS1 from "../components/classes-s1";
+import ClassesS2 from "../components/classes-s2";
+import ClassesS3 from "../components/classes-s3";
 
 class Classes extends Component{
     render(){
         return(        
-            <Container fluid className="classes-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <ClassesS1/>
+                <ClassesS2/>
+                <ClassesS3/>
             </Container>    
         );
     }
